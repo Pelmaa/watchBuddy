c218b2881f4dd6f2a1b1d537772bbe57f9e1d2c8
Pema Wangchuk <pewangk703@gmail.com>
2025-06-22 19:45:59 +0600

    Fix: add vite to dependencies

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import _axios from "axios";
 
 const axios = _axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 axios.interceptors.request.use(
